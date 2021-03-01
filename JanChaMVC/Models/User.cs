b95bbe344aedc53f2b22cbb14deba5a98cb4cf53
点豆豆 <@NICE>
2021-03-01 20:20:59 +0800

    1

--- JanChaMVC/Models/User.cs
+++ JanChaMVC/Models/User.cs
@@ -12,9 +12,12 @@ namespace JanChaMVC.Models
         public string UserName { get; set; }
         public bool   UserSex   { get; set; }
         public int    Age       { get; set; }
-        public string Phone      { get; set; }
-        public string Number    { get; set; }
-        public string Password { get; set; }
+        public DateTime Time1 { get; set; }
+        public string Phone { get; set; }//手机号
+        public int Emile { get; set; }//电子邮件
+        public string Note { get; set; }//备注
+        public string Number { get; set; }//账号
+        public string Password { get; set; }//密码
     }
 
 }
