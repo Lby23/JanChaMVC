43ad4d040c404b769197cf63d89bc099b7f90e19
Lby23 <2365064920@qq.com>
2021-03-08 10:21:13 +0800

    界面

---
+++ JanChaMVC/Areas/Admin/Controllers/DongChaController1.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JanChaMVC.Areas.Admin.Controllers
+{
+    public class DongChaController1 : Controller
+    {
+        [Area("Admin")]
+        public IActionResult Index()//要闻简讯
+        {
+            return View();
+        }
+
+        [Area("Admin")]
+        public IActionResult Index1()//文章详情
+        {
+            return View();
+        }
+
+        [Area("Admin")]
+        public IActionResult Index2()//调查问卷
+        {
+            return View();
+        }
+        [Area("Admin")]
+        public IActionResult Index4()//联系我们
+        {
+            return View();
+        }
+    }
+}
